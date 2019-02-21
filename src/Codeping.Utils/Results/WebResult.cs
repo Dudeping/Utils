3eb98e2efbce1c31d088e95d12e2dbbffdee3aa6
codeping <deping.du@gmail.com>
2019-02-22 07:47:51 +0800

    封装 web 通用返回

---
+++ src/Codeping.Utils/Results/WebResult.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeping.Utils
+{
+    public static class WebResult
+    {
+        public static ContentResult JContent(this PageModel page, string text, string url)
+        {
+            return page.Content($"<script>alert('{text}'); location.href='{url}';</script>", "text/html", Encoding.UTF8);
+        }
+
+        public static ContentResult JContent(this ControllerBase controller, string text, string url)
+        {
+            return controller.Content($"<script>alert('{text}'); location.href='{url}';</script>", "text/html", Encoding.UTF8);
+        }
+
+        public static ContentResult JContent(this ControllerBase controller, OperatResult result, string successUrl, string failUrl)
+        {
+            return controller.JContent(result.IsError ? result.ErrorMessage : "成功!", result.IsError ? failUrl : successUrl);
+        }
+    }
+}
