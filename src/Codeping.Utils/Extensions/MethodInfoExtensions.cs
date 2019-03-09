74500bc6a16ddb0e824cb514e543284bfcec85cb
codeping <deping.du@gmail.com>
2019-03-10 01:35:11 +0800

    更新打包信息

---
+++ src/Codeping.Utils/Extensions/MethodInfoExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Codeping.Utils
+{
+    public static class MethodInfoExtensions
+    {
+        public static string GetFullName(this MethodInfo method)
+        {
+            return $"{method.DeclaringType.FullName}.{method.Name}";
+        }
+    }
+}
