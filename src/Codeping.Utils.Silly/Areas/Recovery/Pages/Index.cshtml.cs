ceaf712e95b98c08824f59c8fb5d9af6a825c473
codeping <deping.du@gmail.com>
2019-03-10 02:18:33 +0800

    添加追讨项目

---
+++ src/Codeping.Utils.Silly/Areas/Recovery/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Codeping.Utils.Silly.Recovery.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
