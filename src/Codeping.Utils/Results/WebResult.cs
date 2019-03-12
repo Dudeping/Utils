86da51e5f3db43376bf09d4e26f2e753f99b7156
codeping <deping.du@gmail.com>
2019-03-13 07:08:59 +0800

    修复提示换行

--- src/Codeping.Utils/Results/WebResult.cs
+++ src/Codeping.Utils/Results/WebResult.cs
@@ -13,11 +13,15 @@ namespace Codeping.Utils
     {
         public static ContentResult JContent(this PageModel page, string text, string url)
         {
+            text = text.Replace('\'', '\0').Replace('\r', '\0').Replace('\n', '\0');
+
             return page.Content($"<script>alert('{text}'); location.href='{url}';</script>", "text/html", Encoding.UTF8);
         }
 
         public static ContentResult JContent(this ControllerBase controller, string text, string url)
         {
+            text = text.Replace('\'', '\0').Replace('\r', '\0').Replace('\n', '\0');
+
             return controller.Content($"<script>alert('{text}'); location.href='{url}';</script>", "text/html", Encoding.UTF8);
         }
 
