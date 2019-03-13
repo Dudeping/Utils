22adddd72a81f19188f97422fbae530c09a4f03b
codeping <deping.du@gmail.com>
2019-03-14 07:40:30 +0800

    添加 TimedJob 单元测试

---
+++ src/Units/TimedJobTests/Writer.cs
@@ -0,0 +1,18 @@
+﻿using Codeping.Utils.TimedJob;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimedJobTests
+{
+    public class Writer : IJob
+    {
+        [Invoke(Interval = 2 * 1000, IsEnabled = true, SkipWhileExecuting = true)]
+        public void Print()
+        {
+            Debug.WriteLine(DateTime.Now);
+        }
+    }
+}
