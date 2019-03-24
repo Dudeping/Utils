2dda3df17d158f2dbd4224d83d34df0c8823d3c5
codeping <deping.du@gmail.com>
2019-03-24 23:26:35 +0800

    添加 Stream 测试

--- src/Codeping.Utils/Extensions/Ext.Stream.cs
+++ src/Codeping.Utils/Extensions/Ext.Stream.cs
@@ -14,7 +14,7 @@ namespace Codeping.Utils
         /// <param name="encoding">字符编码</param>
         /// <param name="bufferSize">缓冲区大小</param>
         /// <param name="isCloseStream">读取完成是否释放流, 默认为true</param>
-        public static string ToString(
+        public static string ToText(
             this Stream stream, Encoding encoding = null, int bufferSize = 1024 * 2, bool isCloseStream = true)
         {
             if (stream == null ||
@@ -53,11 +53,11 @@ namespace Codeping.Utils
         /// <param name="encoding">字符编码</param>
         /// <param name="bufferSize">缓冲区大小</param>
         /// <param name="isCloseStream">读取完成是否释放流, 默认为true</param>
-        public static async Task<string> ToStringAsync(
+        public static async Task<string> ToTextAsync(
             this Stream stream, Encoding encoding = null, int bufferSize = 1024 * 2, bool isCloseStream = true)
         {
             if (stream == null ||
-                stream.CanRead)
+                !stream.CanRead)
             {
                 return string.Empty;
             }
@@ -90,10 +90,11 @@ namespace Codeping.Utils
         /// </summary>
         /// <param name="stream">流</param>
         /// <param name="encoding">字符编码</param>
-        public static async Task<string> CopyToStringAsync(this Stream stream, Encoding encoding = null)
+        public static string CopyToText(
+            this Stream stream, Encoding encoding = null, bool isCloseStream = true)
         {
             if (stream == null ||
-                stream.CanRead)
+                !stream.CanRead)
             {
                 return string.Empty;
             }
@@ -104,7 +105,52 @@ namespace Codeping.Utils
             }
 
             using (MemoryStream ms = new MemoryStream())
-            using (StreamReader sr = new StreamReader(ms, encoding))
+            using (StreamReader sr = new StreamReader(ms, encoding, !isCloseStream))
+            {
+                if (stream.CanSeek)
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+                }
+
+                stream.CopyTo(ms);
+
+                if (ms.CanSeek)
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                }
+
+                string result = sr.ReadToEnd();
+
+                if (stream.CanSeek)
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+                }
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 复制流并转换成字符串
+        /// </summary>
+        /// <param name="stream">流</param>
+        /// <param name="encoding">字符编码</param>
+        public static async Task<string> CopyToTextAsync(
+            this Stream stream, Encoding encoding = null, bool isCloseStream = true)
+        {
+            if (stream == null ||
+                !stream.CanRead)
+            {
+                return string.Empty;
+            }
+
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+
+            using (MemoryStream ms = new MemoryStream())
+            using (StreamReader sr = new StreamReader(ms, encoding, !isCloseStream))
             {
                 if (stream.CanSeek)
                 {
--- src/Units/Utils.Tests/Ext/ExtStreamTest.cs
+++ src/Units/Utils.Tests/Ext/ExtStreamTest.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using Codeping.Utils;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Utils.Tests
 {
-    class ExtStreamTest
+    public class ExtStreamTest
     {
+        [Fact]
+        public async Task StreamTestAsync()
+        {
+            using (var ms = new MemoryStream("hello".ToBytes()))
+            {
+                Assert.Equal("hello", ms.ToText(isCloseStream: false));
+                Assert.Equal("hello", ms.CopyToText(isCloseStream: false));
+                Assert.Equal("hello", await ms.ToTextAsync(isCloseStream: false));
+                Assert.Equal("hello", await ms.CopyToTextAsync(isCloseStream: false));
+            }
+
+            Stream stream = null;
+
+            Assert.Equal("", stream.ToText());
+            Assert.Equal("", await stream.ToTextAsync());
+            Assert.Equal("", stream.CopyToText());
+            Assert.Equal("", await stream.CopyToTextAsync());
+        }
     }
 }
