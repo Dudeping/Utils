d2b0ed2deb33806979f76fa2bf9f46ad6602d8be
codeping <deping.du@gmail.com>
2019-04-06 09:24:37 +0800

    独立敏捷项目

---
+++ src/Codeping.Agile/Codeping.Utils.ExcelMapper/ExcelConvert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codeping.Utils.ExcelMapper
+{
+    public static class ExcelConvert
+    {
+        //public IEnumerable<T> Read<T>()
+        //{
+
+        //}
+
+        //public IEnumerable<T> Read<T1, T2>()
+        //{
+
+        //}
+    }
+}
