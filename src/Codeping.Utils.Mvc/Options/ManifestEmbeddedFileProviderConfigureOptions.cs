76112adb97baa84a85c0f7832593fee3b83de425
codeping <deping.du@gmail.com>
2019-04-21 23:30:35 +0800

    添加嵌入文件提供程序

--- src/Codeping.Utils.Mvc/Options/ManifestEmbeddedFileProviderConfigureOptions.cs
+++ src/Codeping.Utils.Mvc/Options/ManifestEmbeddedFileProviderConfigureOptions.cs
@@ -7,28 +7,37 @@ using System;
 
 namespace Codeping.Utils.Mvc
 {
-    public abstract class ManifestEmbeddedFileConfigureOptions : IPostConfigureOptions<StaticFileOptions>
+    /// <summary>
+    /// 嵌入资源提供程序配置选项, 将指定嵌入资源的以作为文件提供程序
+    /// 使用：services.ConfigureOptions<GinkUIConfigureOptions>();
+    /// 注意：一定要在 AddMvc 之前注入!
+    /// </summary>
+    public abstract class ManifestEmbeddedFileProviderConfigureOptions : IPostConfigureOptions<StaticFileOptions>
     {
         protected readonly IHostingEnvironment _host;
 
-        public ManifestEmbeddedFileConfigureOptions(IHostingEnvironment host)
+        public ManifestEmbeddedFileProviderConfigureOptions(IHostingEnvironment host)
         {
             _host = host;
         }
 
+        /// <summary>
+        /// 资源所在根路径
+        /// </summary>
         protected abstract string Root { get; }
 
         public void PostConfigure(string name, StaticFileOptions options)
         {
-            name = name ?? throw new ArgumentNullException(nameof(name));
-            options = options ?? throw new ArgumentNullException(nameof(options));
+            // see https://www.cnblogs.com/sheng-jie/p/9165547.html
 
-            options.ContentTypeProvider ??= new FileExtensionContentTypeProvider();
+            options = options ?? throw new ArgumentNullException(nameof(options));
 
             if (options.FileProvider == null && _host.WebRootFileProvider == null)
             {
-                throw new InvalidOperationException("Missing FileProvider.");
+                throw new InvalidOperationException("找不到文件提供程序!");
             }
+
+            options.ContentTypeProvider ??= new FileExtensionContentTypeProvider();
 
             options.FileProvider ??= _host.WebRootFileProvider;
 
