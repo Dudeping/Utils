9577148c663c69e0520987e22d9220a570d99b7d
codeping <deping.du@gmail.com>
2019-05-12 20:56:56 +0800

    修改控制台扩展测试

--- src/Units/Codeping.ConsoleEx.Tests/Program.cs
+++ src/Units/Codeping.ConsoleEx.Tests/Program.cs
@@ -7,47 +7,13 @@ namespace ConsoleExTest
     {
         static void Main(string[] args)
         {
-            var dt = DateTime.Now;
+            BasicTest();
 
-            Console.WriteLine(dt);
-            Console.WriteLine(dt.ToLongDateString());
-            Console.WriteLine(dt.ToFileTime());
-            Console.WriteLine(dt.ToFileTimeUtc());
-            Console.WriteLine(dt.ToLocalTime());
-            Console.WriteLine(dt.ToLongDateString());
-            Console.WriteLine(dt.ToLongTimeString());
-            Console.WriteLine(dt.ToOADate());
-            Console.WriteLine(dt.ToShortDateString());
-            Console.WriteLine(dt.ToShortTimeString());
-            Console.WriteLine(dt.ToUniversalTime());
+            Console.ReadKey();
+        }
 
-
-            Console.WriteLine(0.025.ToString("00%"));
-
-            Console.WriteLine(string.Format("{0:P2}", 0.25));
-
-            Console.WriteLine(0.12.ToString("P2"));
-            Console.WriteLine(0.122.ToString("P2"));
-            Console.WriteLine(0.1221.ToString("P2"));
-            Console.WriteLine(0.1220.ToString("P2"));
-            Console.WriteLine(0.0225.ToString("P2"));
-
-            Console.WriteLine("---------");
-
-            Console.WriteLine(0.12.ToString("##.##%"));
-            Console.WriteLine(0.122.ToString("##.##%"));
-            Console.WriteLine(0.1221.ToString("##.##%"));
-            Console.WriteLine(0.1220.ToString("##.##%"));
-            Console.WriteLine(0.025.ToString("##.##%"));
-
-            Console.WriteLine("---------");
-
-            Console.WriteLine(0.12.ToString("00.00%"));
-            Console.WriteLine(0.122.ToString("00.00%"));
-            Console.WriteLine(0.1221.ToString("00.00%"));
-            Console.WriteLine(0.1220.ToString("00.00%"));
-            Console.WriteLine(0.025.ToString("00.00%"));
-
+        static void BasicTest()
+        {
             ConsoleEx.CoverCurrentLine(8, ConsoleColor.DarkYellow);
 
             ConsoleEx.WriteLine("6000005555555555066", ConsoleColor.DarkRed);
@@ -56,8 +22,15 @@ namespace ConsoleExTest
             ConsoleEx.CoverCurrentLine(80, ConsoleColor.DarkGreen);
 
             Console.Write(Console.WindowWidth);
+        }
 
-            Console.ReadKey();
+        static void MuliTest()
+        {
+
+        }
+
+        static void ProgressTest()
+        {
         }
     }
 }
