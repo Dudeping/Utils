b149a0e376e9cb4a9f7e67ed62e8d7b468f63aa5
codeping <deping.du@gmail.com>
2019-05-12 20:18:47 +0800

    添加控制台扩展

---
+++ src/Codeping.Utils/Helpers/ConsoleEx/IProgressor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Codeping.Utils
+{
+    public interface IProgressor
+    {
+        /// <summary>
+        /// 输出进度
+        /// </summary>
+        /// <param name="rate">当前进度</param>
+        void Progress(int rate);
+        /// <summary>
+        /// 使用指定的颜色输出数据, 并在输入完成后输入换行符, 输入完成后会将颜色复位
+        /// </summary>
+        /// <param name="message">要输出的文字</param>
+        /// <param name="color">指定的文字颜色</param>
+        void Finished(string message, ConsoleColor color = ConsoleColor.White);
+        /// <summary>
+        /// 使用指定的颜色输出数据, 并在输入完成后输入换行符, 输入完成后会将颜色复位
+        /// </summary>
+        /// <param name="message">要输出的文字</param>
+        /// <param name="foregroundColor">指定的文字颜色</param>
+        /// <param name="backgroundColor">指定的背景颜色</param>
+        void Finished(
+            string message,
+            ConsoleColor foregroundColor = ConsoleColor.White,
+            ConsoleColor backgroundColor = ConsoleColor.Black);
+    }
+}
---
+++ src/Codeping.Utils/Models/PrintStyle.cs
@@ -0,0 +1,10 @@
+﻿namespace Codeping.Utils
+{
+    public class PrintStyle
+    {
+        public const string Arrow = "=>";
+        public const string CSharp = "#";
+        public const string Point = ".";
+        public const string Plus = "+";
+    }
+}
