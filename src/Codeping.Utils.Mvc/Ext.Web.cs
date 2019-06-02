2afad9ffd61fa015923557cfef2f5ba7d7693183
codeping <deping.du@gmail.com>
2019-06-02 23:54:53 +0800

    修复扩展

--- src/Codeping.Utils.Mvc/Ext.Web.cs
+++ src/Codeping.Utils.Mvc/Ext.Web.cs
@@ -1,23 +1,29 @@
 ﻿using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Net.Http.Headers;
 
 namespace Codeping.Utils.Mvc
 {
     public static partial class Ext
     {
-        public static ContentResult Message(this IFilterMetadata page, string text, string url)
+        public static ContentResult Message(this PageModel _, string text, string url)
         {
             text = text.Replace('\'', '\0').Replace('\r', '\0').Replace('\n', '\0');
 
+            var mediaType = MediaTypeHeaderValue.Parse("text/html");
+
+            mediaType.Encoding = Encoding.UTF8;
+
             return new ContentResult
             {
+                ContentType = mediaType.ToString(),
                 Content = $"<script>alert('{text}'); location.href='{url}';</script>",
-                ContentType = "text/html",
             };
         }
 
-        public static ContentResult Message(this IFilterMetadata page, Result result, string successUrl, string failUrl)
+        public static ContentResult Message(this PageModel page, Result result, string successUrl, string failUrl)
         {
             return page.Message(result.Succeeded ? result.ErrorMessage : "操作成功!", result.Succeeded ? failUrl : successUrl);
         }
